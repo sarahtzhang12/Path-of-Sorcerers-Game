00106f117fae28ddd513c3a1acda9c1091a326e4
sarahtzhang12 <159182521+sarahtzhang12@users.noreply.github.com>
2025-01-15 11:19:13 -0800

    L4 & L5 Player Movement & Bullet

---
+++ M10.twin_stick_workbook/Player.gd
@@ -0,0 +1,11 @@
+class_name Player extends CharacterBody2D
+
+@export var speed := 460.0
+@export var drag_factor := 10.0
+
+func _physics_process(delta: float) -> void:
+	var move_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var desired_velocity := speed * move_direction
+	var steering := desired_velocity - velocity
+	velocity += steering * drag_factor * delta
+	move_and_slide()
