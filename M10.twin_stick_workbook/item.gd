d66f007e396d672d0e130ec92820fef49d653844
sarahtzhang12 <159182521+sarahtzhang12@users.noreply.github.com>
2025-01-31 11:54:39 -0800

    L11

---
+++ M10.twin_stick_workbook/item.gd
@@ -0,0 +1,8 @@
+class_name Item extends Resource
+
+@export var display_name := ""
+@export var texture: Texture2D = null
+@export var sound_on_pickup: AudioStream = null
+
+func use(player: Player) -> void:
+	pass
